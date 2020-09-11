577852cd3c9caf90df81d31e38734abf808652d7
Darren <yong990808@gmail.com>
2020-09-11 23:08:27 +0800

    Created a component for trigger node

---
+++ src/components/trigger_node.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="wrapper_container">
+        <div data-toggle="modal" data-target="#triggerModal" class="trigger_wrapper trigger_text_wrapper">
+            <p class="add_trigger_text">
+                <img class="trigger_plus_icon" src="@/assets/plus.svg">
+                Add an entry point
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "triggerNode",
+}
+</script>
+
+<style>
+
+</style>
