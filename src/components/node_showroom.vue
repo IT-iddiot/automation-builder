ff23dca791e4c636276bfe217117ecef34d29bca
Darren <yong990808@gmail.com>
2020-09-11 23:07:45 +0800

    Included a node_wrapper  showroom for references purpose only

---
+++ src/components/node_showroom.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="left_panel">
+
+        <trigger-node></trigger-node>
+
+        <node-wrapper 
+            v-for="data in node_data" 
+            :key="data.index"
+            :details="data"
+            condition="whatever"
+        />
+
+    </div>
+</template>
+
+<script>
+import triggerNode from './trigger_node.vue';
+import nodeWrapper from '../components/node_wrapper';
+import node_data from '../js/node.js';
+
+export default {
+    name: "nodeShowroom",
+    components : {
+        nodeWrapper,
+        triggerNode
+    },
+    data() {
+        return {
+            node_data : node_data,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
