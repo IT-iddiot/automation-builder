d636576d950670bd2d02eb30ed6688fa51cfafc7
Darren <yong990808@gmail.com>
2020-09-16 12:29:36 +0800

    Created component for add step button

---
+++ src/components/addStepButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button id="add_actions_button" class="btn btn-secondary" data-toggle="modal" data-target="#addStep">
+            <i class="fas fa-plus flex_center"></i>
+        </button>    
+    </div>
+</template>
+
+<script>
+export default {
+    name : "addNodeButton"
+}
+</script>
+
+<style lang="scss" scoped>
+
+#add_actions_button {
+    width : 16px;
+    height : 16px;
+    padding : 0;
+    border-radius : 50%;
+}
+
+#add_actions_button i {
+    font-size : 9px;
+}
+
+</style>
